fe663c3daa0feed9dc8e93687e873298a495819d
Hossein <smh.miraftabi1373@gmail.com>
2019-06-10 10:16:21 +0100

    add joke controller

---
+++ src/main/java/hossein/springframework/joke/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package hossein.springframework.joke.controllers;
+
+import hossein.springframework.joke.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping(value={"/",""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
