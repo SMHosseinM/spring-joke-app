b99d4b59f93a035f1363385d7478a68f72cd5025
Hossein <smh.miraftabi1373@gmail.com>
2019-06-11 09:46:43 +0100

    add java config. Closes #1

--- src/main/java/hossein/springframework/joke/JokeappApplication.java
+++ src/main/java/hossein/springframework/joke/JokeappApplication.java
@@ -2,6 +2,7 @@ package hossein.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class JokeappApplication {
